14c9b3cf65f5352ec27e5780b096549959658fe9
IvanRusinek <38792226+IvanRusinek@users.noreply.github.com>
2019-04-24 15:08:33 -0300

    Initial commit

---
+++ Js/menu.js
@@ -0,0 +1,8 @@
+/*js para el menu*/
+
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener ('click', function () {
+'use strict';
+menu.classList.toggle('mostrar');
+});
